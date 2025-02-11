283e53e8f6694a55b1bba4503c30b44879a0d57a
Walter Schulze <awalterschulze@gmail.com>
2025-02-11 10:14:30 +0000

    cleanup and remove everything except the jessicacantswim game

---
+++ src/JessicaCantSwim/Draw.lean
@@ -0,0 +1,11 @@
+import JessicaCantSwim.Colors
+import JessicaCantSwim.Shape
+
+namespace Draw
+
+inductive Draw where
+| Text (text: String) (x: Nat) (y: Nat) (size: Nat) (color: Colors.Color)
+| Rectangle (r: Shape.Rectangle) (color: Colors.Color)
+| Circle (circle: Shape.Circle) (color: Colors.Color)
+
+end Draw
